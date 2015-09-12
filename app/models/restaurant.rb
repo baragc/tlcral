ce58277f2091957213f36b3324b391e115058070
tramin <clicktam1@gmail.com>
2015-09-12 16:29:48 +0700

    Temporarily remove images from homepage

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,12 +1,7 @@
 class Restaurant < ActiveRecord::Base
-	if Rails.env.development?
-		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.gif"
-  	else
-    	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.gif",
-        :storage => :dropbox,
-        :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-        :path => ":style/:id_:filename"
-  	end
-
+	has_attached_file :image, style: {image: "200x", thumb: "100x100"}, default_url: "default.gif",
+      :storage => :dropbox,
+      :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+      
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end

35af7995b0e9c2be545f46b5af116113d7b4d3d9
tramin <clicktam1@gmail.com>
2015-09-12 16:52:22 +0700

    Finish setup for Dropbox 3

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,12 +1,12 @@
 class Restaurant < ActiveRecord::Base
 
 	if Rails.env.development?
-		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.gif"
+		has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.gif"
 	else
-		has_attached_file :image, style: {image: "200x", thumb: "100x100"}, default_url: "default.gif",
+		has_attached_file :image, styles: {image: "200x", thumb: "100x100"}, default_url: "default.gif",
 		:storage => :dropbox,
       	:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-      	:path => ":tlcral/:id_:filename"
+      	:path => "tlcral/:id_:filename"
 	end
 
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
